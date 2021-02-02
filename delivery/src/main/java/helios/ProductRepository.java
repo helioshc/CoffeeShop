7f26a2e4f43782e867cbad9ca482ee8aabad8c95
helioshc <64818523+helioshc@users.noreply.github.com>
2021-02-02 19:25:46 +0900

    Delete ProductRepository.java

--- delivery/src/main/java/helios/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package helios;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ProductRepository extends PagingAndSortingRepository<Product, Long>{
-
-
-}
