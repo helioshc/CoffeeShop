0bdbc0772d27577703b814a8de1c1b3352cc2cef
helioshc <64818523+helioshc@users.noreply.github.com>
2021-02-23 00:09:36 +0900

    Update ProductService.java

--- cafe/src/main/java/coffeeshop/external/ProductService.java
+++ cafe/src/main/java/coffeeshop/external/ProductService.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-// import java.util.Date;
+import java.util.Date;
 
-//@FeignClient(name="kitchen", url="${api.url.kitchen}")     // phone82 ÂüÁ¶
-@FeignClient(name="kitchen", url="http://kitchen:8080") // origin Src
+//@FeignClient(name="kitchen", url="${api.url.kitchen}")
+@FeignClient(name="kitchen", url="http://kitchen:8080") 
 public interface ProductService {
 
-    @RequestMapping(method= RequestMethod.PUT, path="/products/{id}")
+    @RequestMapping(method= RequestMethod.POST, path="/products/{id}")
     public void cancel(@PathVariable("id") Long productId, @RequestBody Product product);
 
 }
