473a120c90d5c556ad315caa8e03e09db653e2ad
helioshc <64818523+helioshc@users.noreply.github.com>
2021-02-23 15:59:15 +0900

    Update MyPagesViewHandler.java

--- customercenter/src/main/java/coffeeshop/MyPagesViewHandler.java
+++ customercenter/src/main/java/coffeeshop/MyPagesViewHandler.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -21,15 +22,16 @@ public class MyPagesViewHandler {
     public void whenOrdered_then_CREATE_1 (@Payload Ordered ordered) {
         try {
             if (ordered.isMe()) {
-                // view 객체 생성
+                // view object create
                 MyPages myPages  = new MyPages();
-                // view 객체에 이벤트의 Value 를 set 함
+                // set Value of event Object to view object
                 myPages.setQty(ordered.getQty());
                 myPages.setStatus(ordered.getStatus());
                 myPages.setProductName(ordered.getProductName());
                 myPages.setOrderId(ordered.getId());
+                myPages.setId(ordered.getId());
                 
-                // view 레파지 토리에 save
+                // view object Save
                 myPagesRepository.save(myPages);
             }
         }catch (final Exception e){
@@ -41,17 +43,17 @@ public class MyPagesViewHandler {
     public void whenProduced_then_UPDATE_1(@Payload Produced produced ) {
         try {
             if (produced.isMe()) {
-                // view 객체 조회
-//                List<MyPages> list = myPagesRepository.findByOrderId(produced.getOrderId());
-                   MyPages myPages = myPagesRepository.findByOrderId(produced.getOrderId()).get();
-            //    for( MyPages myPages : list){
-                    // view 객체에 이벤트의 eventDirectValue 를 set 함
-                    myPages.setProductId(produced.getProductId());
+                // view 
+                // List<MyPages> list = myPagesRepository.findByOrderId(produced.getOrderId());
+                   MyPages myPages = myPagesRepository.findByOrderId(produced.getOrderId());
+                //for( MyPages myPages : list){
+                    // event object's DirectValue to view
+                    // myPages.setProductId(produced.getProductId());
                     myPages.setStatus(produced.getStatus());
                     //myPages.setStatus("Produced");
-                    // view 레파지토리에 save
+                    // view save
                     myPagesRepository.save(myPages);
-            //    }
+                // }
             }
         }catch (Exception e){
             e.printStackTrace();
@@ -62,33 +64,31 @@ public class MyPagesViewHandler {
     public void whenProductCanceled_then_UPDATE_2(@Payload ProductCanceled productCanceled) {
         try {
             if (productCanceled.isMe()) {
-                // view 객체 조회
-//                List<> List = myPagesRepository.findByOrderId(productCanceled.getOrderId());
-                MyPages myPages = myPagesRepository.findByOrderId(productCanceled.getOrderId()).get();
-            //    for( MyPages myPages : List){
-                    // view 객체에 이벤트의 eventDirectValue 를 set 함
-                    myPages.setProductId(productCanceled.getProductId());
+                // view
+                // List<> List = myPagesRepository.findByOrderId(productCanceled.getOrderId());
+                MyPages myPages = myPagesRepository.findByOrderId(productCanceled.getOrderId());
+                //for( MyPages myPages : List){
+                    // view eventDirectValue 
+                    // myPages.setProductId(productCanceled.getProductId());
                     myPages.setStatus(productCanceled.getStatus());
-                    // view 레파지토리에 save
+                    // view save
                     myPagesRepository.save(myPages);
-            //    }
+                //}
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whenOrderCanceled_then_DELETE_1(@Payload OrderCanceled orderCanceled) {
+        try {
+            if (orderCanceled.isMe()) {
+                // delete view list by ID
+                myPagesRepository.deleteByOrderId(orderCanceled.getId());
             }
         }catch (Exception e){
             e.printStackTrace();
         }
     }
 }
-
-/*
-    @StreamListener(KafkaProcessor.INPUT)
-    public void whenOrderCanceled_then_DELETE_1(@Payload OrderCanceled orderCanceled) {
-        try {
-            if (orderCanceled.isMe()) {
-                // view 레파지 토리에 삭제 쿼리
-                MyPagesRepository.deleteByOrderId(orderCanceled.getId());
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-   */ 
